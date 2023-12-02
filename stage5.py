22cc55fdd0e000dfc7b5ae7ef92fd15bc3254103
Emily Ingbertsen <eaingbertsen@gmail.com>
2023-12-02 11:49:28 -0700

    added data

--- stage5.py
+++ stage5.py
@@ -1,39 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-task1Misclicks = np.array([])
-task1Times = np.array([])
-task1Success = np.array([])
+#In the order of T,K,J,H,D
 
-task2Misclicks = np.array([])
-task2Times = np.array([])
-task2Success = np.array([])
+task1Misclicks = np.array([0,0,0,0,0])
+task1Times = np.array([24,13,10,8,10]) #in seconds
+task1Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-task3Misclicks = np.array([])
-task3Times = np.array([])
-task3Success = np.array([])
+task2Misclicks = np.array([0,0,0,2,0])
+task2Times = np.array([60,24,16,32,13])
+task2Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-task4Misclicks = np.array([])
-task4Times = np.array([])
-task4Success = np.array([])
+task3Misclicks = np.array([2,0,0,0,0])
+task3Times = np.array([20,15,9,17,8])
+task3Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-task5Misclicks = np.array([])
-task5Times = np.array([])
-task5Success = np.array([])
+task4Misclicks = np.array([2,0,0,0,3])
+task4Times = np.array([26,3,3,9,8])
+task4Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-task6Misclicks = np.array([])
-task6Times = np.array([])
-task6Success = np.array([])
+task5Misclicks = np.array([0,0,0,0,0])
+task5Times = np.array([6,4,2,5,13])
+task5Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-task7Misclicks = np.array([])
-task7Times = np.array([])
-task7Success = np.array([])
+task6Misclicks = np.array([0,0,0,0,0])
+task6Times = np.array([11,4,2,6,8])
+task6Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-task8Misclicks = np.array([])
-task8Times = np.array([])
-task8Success = np.array([])
+task7Misclicks = np.array([0,0,0,0,0])
+task7Times = np.array([5,3,2,4,7])
+task7Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-users = []
+task8Misclicks = np.array([3,0,0,3,1])
+task8Times = np.array([85,27,12,47,21])
+task8Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
 misclickLabels = []
 timesLabels = []
