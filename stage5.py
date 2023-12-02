73abb8f552d44cca1924243a5063bd22cf87ea25
Emily Ingbertsen <eaingbertsen@gmail.com>
2023-12-02 13:28:00 -0700

    finished

--- stage5.py
+++ stage5.py
@@ -35,15 +35,73 @@ task8Misclicks = np.array([3,0,0,3,1])
 task8Times = np.array([85,27,12,47,21])
 task8Success = np.array(["Yes","Yes","Yes","Yes","Yes"])
 
-misclickLabels = []
-timesLabels = []
-successLabels = []
 
-# Calculate Mean:
-# numpy.mean(some array)
+task1TimeMean = np.mean(task1Times)
+task1TimeSTD = np.std(task1Times)
+print("Task 1 Average Time to Complete: ",task1TimeMean)
+print("Task 1 STD of Time to Complete: ",task1TimeSTD)
 
-# Calculate Standard Deviation:
-# numpy.std(some array)
+task2TimeMean = np.mean(task2Times)
+task2TimeSTD = np.std(task2Times)
+print("Task 2 Average Time to Complete: ",task2TimeMean)
+print("Task 2 STD of Time to Complete: ",task2TimeSTD)
 
-#plt.pie(data, labels = mylabels)
-#plt.show() 
+task3TimeMean = np.mean(task3Times)
+task3TimeSTD = np.std(task3Times)
+print("Task 3 Average Time to Complete: ",task3TimeMean)
+print("Task 3 STD of Time to Complete: ",task3TimeSTD)
+
+task4TimeMean = np.mean(task4Times)
+task4TimeSTD = np.std(task4Times)
+print("Task 4 Average Time to Complete: ",task4TimeMean)
+print("Task 4 STD of Time to Complete: ",task4TimeSTD)
+
+task5TimeMean = np.mean(task5Times)
+task5TimeSTD = np.std(task5Times)
+print("Task 5 Average Time to Complete: ",task5TimeMean)
+print("Task 5 STD of Time to Complete: ",task5TimeSTD)
+
+task6TimeMean = np.mean(task6Times)
+task6TimeSTD = np.std(task6Times)
+print("Task 6 Average Time to Complete: ",task6TimeMean)
+print("Task 6 STD of Time to Complete: ",task6TimeSTD)
+
+task7TimeMean = np.mean(task7Times)
+task7TimeSTD = np.std(task7Times)
+print("Task 7 Average Time to Complete: ",task7TimeMean)
+print("Task 7 STD of Time to Complete: ",task7TimeSTD)
+
+task8TimeMean = np.mean(task8Times)
+task8TimeSTD = np.std(task8Times)
+print("Task 8 Average Time to Complete: ",task8TimeMean)
+print("Task 8 STD of Time to Complete: ",task8TimeSTD)
+
+timeData = [task1Times,task2Times,task3Times,task4Times,task5Times,task6Times,task7Times,task8Times]
+fig = plt.figure(figsize =(10, 7))
+ax = fig.add_subplot(111)
+bp = ax.boxplot(timeData, vert = 0,showmeans=True)
+
+ax.set_title('Task Times')
+ax.set_xlabel('Total Time to Complete Task in Seconds')
+ax.set_ylabel('Task Number')
+
+plt.show()
+
+misclickData = [task1Misclicks,task2Misclicks,task3Misclicks,task4Misclicks,task5Misclicks,task6Misclicks,task7Misclicks,task8Misclicks]
+
+misclickSums = []
+for list in misclickData:
+    sum = 0
+    for num in list:
+        sum += num
+    misclickSums.append(sum)
+
+
+misclickLabels = ["Task 1","Task 2","Task 3","Task 4","Task 5","Task 6","Task 7","Task 8"]
+
+plt.bar(misclickLabels,misclickSums)
+
+plt.title("Total Number of Misclicks per Task")
+plt.ylabel("Number of Misclicks")
+
+plt.show()
