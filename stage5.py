8921acd9082b5aaaf171eda520ef960aefb1b52e
Emily Ingbertsen <eaingbertsen@gmail.com>
2023-12-02 09:29:21 -0700

    initial setup

---
+++ stage5.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+task1Misclicks = np.array([])
+task1Times = np.array([])
+task1Success = np.array([])
+
+task2Misclicks = np.array([])
+task2Times = np.array([])
+task2Success = np.array([])
+
+task3Misclicks = np.array([])
+task3Times = np.array([])
+task3Success = np.array([])
+
+task4Misclicks = np.array([])
+task4Times = np.array([])
+task4Success = np.array([])
+
+task5Misclicks = np.array([])
+task5Times = np.array([])
+task5Success = np.array([])
+
+task6Misclicks = np.array([])
+task6Times = np.array([])
+task6Success = np.array([])
+
+task7Misclicks = np.array([])
+task7Times = np.array([])
+task7Success = np.array([])
+
+task8Misclicks = np.array([])
+task8Times = np.array([])
+task8Success = np.array([])
+
+users = []
+
+# Calculate Mean:
+# numpy.mean(some array)
+
+# Calculate Standard Deviation:
+# numpy.std(some array)
+
+plt.pie(y, labels = mylabels)
+plt.show() 
